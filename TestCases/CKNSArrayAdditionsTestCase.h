8153fc58d7584eae61fc1a3282d54439204516fb
Fred Brunel <fbrunel@gmail.com>
2009-11-06 16:39:49 -0500

    Added Unit Tests (via SenTestingKit). No real test cases implemented for now.

---
+++ TestCases/CKNSArrayAdditionsTestCase.h
@@ -0,0 +1,15 @@
+//
+//  CKNSArrayAdditionsTestCase.h
+//  CloudKit
+//
+//  Created by Fred Brunel on 09-11-06.
+//  Copyright 2009 WhereCloud Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CKNSArrayAdditionsTestCase : SenTestCase {
+	NSMutableArray *_array;
+}
+
+@end
