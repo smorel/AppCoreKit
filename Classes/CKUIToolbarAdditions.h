00489fef63a9f04bf35b9bbe8e7cdaacd270bdd3
Fred Brunel <fbrunel@gmail.com>
2010-05-17 15:08:31 -0400

    Add CKUIToolbarAdditions with -replaceItemWithTag:withItem:

---
+++ Classes/CKUIToolbarAdditions.h
@@ -0,0 +1,15 @@
+//
+//  CKUIToolbarAdditions.h
+//  CloudKit
+//
+//  Created by Fred Brunel on 10-05-17.
+//  Copyright 2010 WhereCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIToolbar (CKUIToolbarAdditions)
+
+- (void)replaceItemWithTag:(NSInteger)tag withItem:(UIBarButtonItem *)item;
+
+@end
