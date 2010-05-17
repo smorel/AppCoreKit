a36cc1101e17dcd3e07d6e79295f66ab6657351c
Fred Brunel <fred@wherecloud.com>
2010-05-17 16:55:48 -0400

    Revert to the original version of CKWebViewController because the new refactoring had an important regression; it would *not* properly support customized image buttons -- changing the color of buttons need to be implemented via UIButton embedded into UIBarButtonItem.
    The current refactored version has been renamed to CKWebViewControllerRef.

--- Classes/CKWebViewController.m
+++ Classes/CKWebViewController.m
@@ -1,6 +1,6 @@
 //
 //  CKWebViewController.m
-//  CloudKit
+//  YellowPages
 //
 //  Created by Olivier Collet on 10-02-03.
 //  Copyright 2010 WhereCloud Inc. All rights reserved.
@@ -9,68 +9,53 @@
 #import "CKWebViewController.h"
 #import "CKUINavigationControllerAdditions.h"
 #import "CKConstants.h"
-#import "CKBundle.h"
-#import "CKUIToolbarAdditions.h"
+
+#define CKBarButtonItemFlexibleSpace [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil] autorelease]
 
 @interface CKWebViewController ()
-
 @property (nonatomic, readwrite, retain) NSURL *homeURL;
-@property (nonatomic, readwrite, retain) UIWebView *webView;
-
-@property (nonatomic, readwrite, retain) UIBarButtonItem *backButtonItem;
-@property (nonatomic, readwrite, retain) UIBarButtonItem *forwardButtonItem;
-@property (nonatomic, readwrite, retain) UIBarButtonItem *refreshButtonItem;
-@property (nonatomic, readwrite, retain) UIBarButtonItem *actionButtonItem;
-@property (nonatomic, readwrite, retain) UIBarButtonItem *spinnerItem;
-
-@property (nonatomic, readwrite, retain) NSDictionary *navigationControllerStyles;
-
+@property (nonatomic, readwrite, retain) UIBarButtonItem *backButton;
+@property (nonatomic, readwrite, retain) UIBarButtonItem *forwardButton;
+@property (nonatomic, readwrite, retain) UIBarButtonItem *reloadButton;
+@property (nonatomic, readwrite, retain) UIActivityIndicatorView *spinner;
+@property (nonatomic, readwrite, retain) NSMutableArray *toolbarButtonsLoading;
+@property (nonatomic, readwrite, retain) NSMutableArray *toolbarButtonsStatic;
 @end
 
-//
 
 @interface CKWebViewController (Private)
-- (void)setBarButtonItem:(UIBarButtonItem *)buttonItem forItemType:(CKWebViewControllerButtonItemType)itemType target:(id)target action:(SEL)action;
+- (void)generateToolbar;
+- (void)updateToolbar;
 @end
 
-//
+
 
 @implementation CKWebViewController
 
 @synthesize homeURL = _homeURL;
-@synthesize webView = _webView;
+@synthesize backButton = _backButton;
+@synthesize forwardButton = _forwardButton;
+@synthesize reloadButton = _reloadButton;
+@synthesize spinner = _spinner;
+@synthesize toolbarButtonsLoading = _toolbarButtonsLoading;
+@synthesize toolbarButtonsStatic = _toolbarButtonsStatic;
 
-@synthesize backButtonItem = _backButtonItem;
-@synthesize forwardButtonItem = _forwardButtonItem;
-@synthesize refreshButtonItem = _refreshButtonItem;
-@synthesize actionButtonItem = _actionButtonItem;
-@synthesize spinnerItem = _spinnerItem;
-
-@synthesize navigationControllerStyles = _navigationControllerStyles;
-@synthesize showDocumentTitle = _showDocumentTitle;
-@synthesize activityIndicatorViewStyle = _activityIndicatorViewStyle;
 
 - (id)initWithURL:(NSURL *)url {
 	if (self = [super init]) {
 		self.homeURL = url;
-		self.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhite;
+		_showURLInTitle = YES;
+
+		// Create the toolbar buttons
+		[self setImage:[UIImage imageNamed:@"CKWebViewController-goBack.png"] forButton:CKWebViewButtonBack];
+		[self setImage:[UIImage imageNamed:@"CKWebViewController-goForward.png"] forButton:CKWebViewButtonForward];
+		self.reloadButton = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(reload)] autorelease];
+		self.spinner = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite] autorelease];
+		
+		[self generateToolbar];
 	}
     return self;	
 }
-
-- (void)dealloc {
-	self.webView = nil;
-	self.homeURL = nil;
-	self.backButtonItem = nil;
-	self.forwardButtonItem = nil;
-	self.refreshButtonItem = nil;
-	self.actionButtonItem = nil;
-	self.spinnerItem = nil;
-	self.navigationControllerStyles = nil;
-    [super dealloc];
-}
-
-#pragma mark View Management
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -78,173 +63,181 @@
 	self.view.autoresizingMask = CKUIViewAutoresizingFlexibleAll;
 
 	// Set up the WebView
-	
-	self.webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
-	self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-	self.webView.scalesPageToFit = YES;
-	self.webView.delegate = self;
-	
-	[self.view addSubview:self.webView];
+	_webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+	_webView.autoresizingMask = CKUIViewAutoresizingFlexibleAll;
+	_webView.scalesPageToFit = YES;
+	_webView.delegate = self;
+	[self.view addSubview:_webView];
 
-	// Setup the bar button items
-	
-	[self setButtonItemWithImage:[CKBundle imageForName:@"CKWebViewControllerGoBack.png"] type:CKWebViewControllerButtonItemBack target:nil action:nil];
-	[self setButtonItemWithImage:[CKBundle imageForName:@"CKWebViewControllerGoForward.png"] type:CKWebViewControllerButtonItemForward target:nil action:nil];
-	[self setButtonItemWithSystemItem:UIBarButtonSystemItemRefresh type:CKWebViewControllerButtonItemRefresh target:nil action:nil];
-	
-	UIActivityIndicatorView *activityView = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:self.activityIndicatorViewStyle] autorelease];
-	[activityView startAnimating];
-	self.spinnerItem = [[[UIBarButtonItem alloc] initWithCustomView:activityView] autorelease];
-	self.spinnerItem.tag = CKWebViewControllerButtonItemRefresh;
-	
-	// Setup the toolbar
-	
-	UIBarButtonItem *fixedSpace = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil] autorelease];
-	fixedSpace.width = 57.0f;
-	UIBarButtonItem *flexiSpace = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil] autorelease];
-	
-	NSMutableArray *items = [NSMutableArray arrayWithObjects:self.backButtonItem, fixedSpace, self.forwardButtonItem, flexiSpace, nil];
-	
-	if (self.actionButtonItem) {
-		[items addObjectsFromArray:[NSArray arrayWithObjects:self.refreshButtonItem, fixedSpace, self.actionButtonItem, nil]];
-	} else {
-		[items addObject:self.refreshButtonItem];
-	}
-	
-	[self setToolbarItems:items];
-	
-	//[NSArray arrayWithObjects:self.backButtonItem, fixedSpace, self.forwardButtonItem, flexiSpace, self.refreshButtonItem, nil] animated:NO];
-	
 	// Load the URL
-
-	NSURLRequest *request = [NSURLRequest requestWithURL:self.homeURL];
-	[self.webView loadRequest:request];
+	if (!_homeURL) return;
+	NSURLRequest *request = [[NSURLRequest alloc] initWithURL:_homeURL];
+	[_webView loadRequest:request];
+	[request release];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
 	[super viewWillAppear:animated];
 
 	// Save the NavigationController styles
-	self.navigationControllerStyles = [self.navigationController getStyles];
+	_navigationControllerStyles = [[self.navigationController getStyles] retain];
 
 	[self.navigationController setNavigationBarHidden:NO animated:animated];
+	
+	[self updateToolbar];
+	[self setToolbarItems:_toolbarButtonsStatic animated:animated];
+
+	// Display the toolbar
 	[self.navigationController setToolbarHidden:NO animated:animated];
 }
 
-- (void)viewWillDisappear:(BOOL)animated {
-	[super viewWillDisappear:animated];
-	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
-	
-	if (self.webView.loading) [self.webView stopLoading];
-	self.webView.delegate = nil;
+- (void)viewDidAppear:(BOOL)animated {
+	[super viewDidAppear:animated];
 
-	// Restore the NavigationController styles
-	[self.navigationController setStyles:self.navigationControllerStyles animated:animated];
-}
-
-- (void)viewDidUnload {
-	self.webView = nil;
-	self.backButtonItem = nil;
-	self.forwardButtonItem = nil;
-	self.refreshButtonItem = nil;
-	self.actionButtonItem = nil;
-	self.spinnerItem = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     return YES;
 }
 
-#pragma mark Public API
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+	
+	if (_webView.loading) [_webView stopLoading];
+	_webView.delegate = nil;
+
+	// Restore the NavigationController styles
+	[self.navigationController setStyles:_navigationControllerStyles animated:animated];
+
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidUnload {
+	[_webView release];
+	_webView = nil;
+	[_backButton release];
+	_backButton = nil;
+	[_forwardButton release];
+	_forwardButton = nil;
+}
+
+- (void)dealloc {
+	[_webView release];
+	[_homeURL release];
+	[_backButton release];
+	[_forwardButton release];
+	[_toolbarButtonsStatic release];
+	[_toolbarButtonsLoading release];
+	[_navigationControllerStyles release];
+    [super dealloc];
+}
+
+- (void)setActionButtonWithStyle:(UIBarButtonSystemItem)style action:(SEL)action target:(id)target {
+	UIBarButtonItem *actionButton = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:style target:target action:action] autorelease];
+	[_toolbarButtonsStatic addObject:CKBarButtonItemFlexibleSpace];
+	[_toolbarButtonsStatic addObject:actionButton];
+	[_toolbarButtonsLoading addObject:CKBarButtonItemFlexibleSpace];
+	[_toolbarButtonsLoading addObject:actionButton];
+}
 
 - (NSURL *)currentURL {
-	return [[NSURL URLWithString:[self.webView stringByEvaluatingJavaScriptFromString:@"window.location.href"]] standardizedURL];
+	return [[NSURL URLWithString:[_webView stringByEvaluatingJavaScriptFromString:@"window.location.href"]] standardizedURL];
 }
 
-- (void)setButtonItemWithSystemItem:(UIBarButtonSystemItem)systemItem type:(CKWebViewControllerButtonItemType)type target:(id)target action:(SEL)action {
-	UIBarButtonItem *item = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:systemItem target:target action:action] autorelease];
-	[self setBarButtonItem:item forItemType:type target:target action:action];
-}
+#pragma mark -
+#pragma mark Toolbar
 
-- (void)setButtonItemWithImage:(UIImage *)image type:(CKWebViewControllerButtonItemType)type target:(id)target action:(SEL)action {
-	if (image == nil)
-		return;
-	UIBarButtonItem *item = [[[UIBarButtonItem alloc] initWithImage:image style:UIBarButtonItemStylePlain target:target action:action] autorelease];
-	[self setBarButtonItem:item forItemType:type target:target action:action];
-}
-
-- (void)setSpinnerStyle:(UIActivityIndicatorViewStyle)style {
-//	self.spinner.activityIndicatorViewStyle = style;
-}
-
-#pragma mark Toolbar Management
-
-- (void)setBarButtonItem:(UIBarButtonItem *)buttonItem forItemType:(CKWebViewControllerButtonItemType)type target:(id)target action:(SEL)action {
+- (void)updateToolbar {
+	_backButton.enabled = _webView.canGoBack;
+	_forwardButton.enabled = _webView.canGoForward;
 	
-	if (target) buttonItem.target = target;
-	if (action) buttonItem.action = action;
+	[self generateToolbar];
+	if ([_webView isLoading]) [self setToolbarItems:self.toolbarButtonsLoading animated:NO];
+	else [self setToolbarItems:self.toolbarButtonsStatic animated:NO];
+}
+
+- (void)goBack {
+	[_webView goBack];
+}
+- (void)goForward {
+	[_webView goForward];
+}
+- (void)reload {
+	[_webView reload];
+}
+
+
+#pragma mark -
+#pragma mark Toolbar Customization
+
+- (void)generateToolbar {
+	self.toolbarButtonsStatic = [[NSMutableArray alloc] initWithObjects:self.backButton, CKBarButtonItemFlexibleSpace, self.forwardButton, CKBarButtonItemFlexibleSpace, CKBarButtonItemFlexibleSpace, CKBarButtonItemFlexibleSpace, self.reloadButton, nil];
+	[self.spinner startAnimating];
+	UIBarButtonItem *loadingItem = [[[UIBarButtonItem alloc] initWithCustomView:self.spinner] autorelease];
+	self.toolbarButtonsLoading = [[NSMutableArray alloc] initWithObjects:self.backButton, CKBarButtonItemFlexibleSpace, self.forwardButton, CKBarButtonItemFlexibleSpace, CKBarButtonItemFlexibleSpace, CKBarButtonItemFlexibleSpace, loadingItem, nil];
+}
+
+- (void)setImage:(UIImage *)image forButton:(CKWebViewButton)button {
+	if (image == nil) return;
+
+	UIButton *btn = [[[UIButton alloc] initWithFrame:CGRectMake(0, 0, 22, 22)] autorelease];
+	[btn setImage:image forState:UIControlStateNormal];
 	
-	switch (type) {
-		case CKWebViewControllerButtonItemBack:
-			self.backButtonItem = buttonItem;
-			self.backButtonItem.target = self;
-			self.backButtonItem.action = @selector(actionGoBack);
-			self.backButtonItem.enabled = NO;
+	switch (button) {
+		case CKWebViewButtonBack:
+			[btn addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+			self.backButton = [[UIBarButtonItem alloc] initWithCustomView:btn];
 			break;
-		case CKWebViewControllerButtonItemForward:
-			self.forwardButtonItem = buttonItem;
-			self.forwardButtonItem.target = self;
-			self.forwardButtonItem.action = @selector(actionGoForward);
-			self.forwardButtonItem.enabled = NO;
+		case CKWebViewButtonForward:
+			[btn addTarget:self action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
+			self.forwardButton = [[UIBarButtonItem alloc] initWithCustomView:btn];
 			break;
-		case CKWebViewControllerButtonItemRefresh:
-			self.refreshButtonItem = buttonItem;
-			self.refreshButtonItem.target = self;
-			self.refreshButtonItem.action = @selector(actionRefresh);
-			self.refreshButtonItem.enabled = YES;
+		case CKWebViewButtonReload:
+			[btn addTarget:self action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+			self.reloadButton = [[UIBarButtonItem alloc] initWithCustomView:btn];
 			break;
-		case CKWebViewControllerButtonItemAction:
-			self.actionButtonItem = buttonItem;
-			self.actionButtonItem.enabled = YES;
 		default:
 			break;
 	}
-	buttonItem.tag = type;	
+	
+	[self updateToolbar];
 }
 
-- (void)actionGoBack {
-	[self.webView goBack];
-}
-- (void)actionGoForward {
-	[self.webView goForward];
-}
-- (void)actionRefresh {
-	[self.webView reload];
+- (void)setSpinnerStyle:(UIActivityIndicatorViewStyle)style {
+	_spinner.activityIndicatorViewStyle = style;
 }
 
+
+#pragma mark -
 #pragma mark WebView Delegate
 
 - (void)webViewDidStartLoad:(UIWebView *)webView {
+	[self updateToolbar];
 	[UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
-	[self.navigationController.toolbar replaceItemWithTag:CKWebViewControllerButtonItemRefresh withItem:self.spinnerItem];
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
 	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+	
+	[self updateToolbar];
 
 	// Update the title
-	if (self.showDocumentTitle) {
-		self.title = [self.webView stringByEvaluatingJavaScriptFromString:@"document.title"];
-	}
-
-	[self.navigationController.toolbar replaceItemWithTag:CKWebViewControllerButtonItemRefresh withItem:self.refreshButtonItem];	
-	
-	self.backButtonItem.enabled = self.webView.canGoBack;
-	self.forwardButtonItem.enabled = self.webView.canGoForward;	
+	if (_showURLInTitle) self.title = [_webView stringByEvaluatingJavaScriptFromString:@"document.title"];
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
 	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+	
+	[self updateToolbar];
 }
+
 
 @end
---
+++ Classes/CKWebViewControllerRef.m
@@ -0,0 +1,250 @@
+//
+//  CKWebViewController.m
+//  CloudKit
+//
+//  Created by Olivier Collet on 10-02-03.
+//  Copyright 2010 WhereCloud Inc. All rights reserved.
+//
+
+#import "CKWebViewControllerRef.h"
+#import "CKUINavigationControllerAdditions.h"
+#import "CKConstants.h"
+#import "CKBundle.h"
+#import "CKUIToolbarAdditions.h"
+
+@interface CKWebViewControllerRef ()
+
+@property (nonatomic, readwrite, retain) NSURL *homeURL;
+@property (nonatomic, readwrite, retain) UIWebView *webView;
+
+@property (nonatomic, readwrite, retain) UIBarButtonItem *backButtonItem;
+@property (nonatomic, readwrite, retain) UIBarButtonItem *forwardButtonItem;
+@property (nonatomic, readwrite, retain) UIBarButtonItem *refreshButtonItem;
+@property (nonatomic, readwrite, retain) UIBarButtonItem *actionButtonItem;
+@property (nonatomic, readwrite, retain) UIBarButtonItem *spinnerItem;
+
+@property (nonatomic, readwrite, retain) NSDictionary *navigationControllerStyles;
+
+@end
+
+//
+
+@interface CKWebViewControllerRef (Private)
+- (void)setBarButtonItem:(UIBarButtonItem *)buttonItem forItemType:(CKWebViewControllerButtonItemType)itemType target:(id)target action:(SEL)action;
+@end
+
+//
+
+@implementation CKWebViewControllerRef
+
+@synthesize homeURL = _homeURL;
+@synthesize webView = _webView;
+
+@synthesize backButtonItem = _backButtonItem;
+@synthesize forwardButtonItem = _forwardButtonItem;
+@synthesize refreshButtonItem = _refreshButtonItem;
+@synthesize actionButtonItem = _actionButtonItem;
+@synthesize spinnerItem = _spinnerItem;
+
+@synthesize navigationControllerStyles = _navigationControllerStyles;
+@synthesize showDocumentTitle = _showDocumentTitle;
+@synthesize activityIndicatorViewStyle = _activityIndicatorViewStyle;
+
+- (id)initWithURL:(NSURL *)url {
+	if (self = [super init]) {
+		self.homeURL = url;
+		self.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhite;
+	}
+    return self;	
+}
+
+- (void)dealloc {
+	self.webView = nil;
+	self.homeURL = nil;
+	self.backButtonItem = nil;
+	self.forwardButtonItem = nil;
+	self.refreshButtonItem = nil;
+	self.actionButtonItem = nil;
+	self.spinnerItem = nil;
+	self.navigationControllerStyles = nil;
+    [super dealloc];
+}
+
+#pragma mark View Management
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+	self.view.autoresizingMask = CKUIViewAutoresizingFlexibleAll;
+
+	// Set up the WebView
+	
+	self.webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+	self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+	self.webView.scalesPageToFit = YES;
+	self.webView.delegate = self;
+	
+	[self.view addSubview:self.webView];
+
+	// Setup the bar button items
+	
+	[self setButtonItemWithImage:[CKBundle imageForName:@"CKWebViewControllerGoBack.png"] type:CKWebViewControllerButtonItemBack target:nil action:nil];
+	[self setButtonItemWithImage:[CKBundle imageForName:@"CKWebViewControllerGoForward.png"] type:CKWebViewControllerButtonItemForward target:nil action:nil];
+	[self setButtonItemWithSystemItem:UIBarButtonSystemItemRefresh type:CKWebViewControllerButtonItemRefresh target:nil action:nil];
+	
+	UIActivityIndicatorView *activityView = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:self.activityIndicatorViewStyle] autorelease];
+	[activityView startAnimating];
+	self.spinnerItem = [[[UIBarButtonItem alloc] initWithCustomView:activityView] autorelease];
+	self.spinnerItem.tag = CKWebViewControllerButtonItemRefresh;
+	
+	// Setup the toolbar
+	
+	UIBarButtonItem *fixedSpace = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil] autorelease];
+	fixedSpace.width = 57.0f;
+	UIBarButtonItem *flexiSpace = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil] autorelease];
+	
+	NSMutableArray *items = [NSMutableArray arrayWithObjects:self.backButtonItem, fixedSpace, self.forwardButtonItem, flexiSpace, nil];
+	
+	if (self.actionButtonItem) {
+		[items addObjectsFromArray:[NSArray arrayWithObjects:self.refreshButtonItem, fixedSpace, self.actionButtonItem, nil]];
+	} else {
+		[items addObject:self.refreshButtonItem];
+	}
+	
+	[self setToolbarItems:items];
+	
+	//[NSArray arrayWithObjects:self.backButtonItem, fixedSpace, self.forwardButtonItem, flexiSpace, self.refreshButtonItem, nil] animated:NO];
+	
+	// Load the URL
+
+	NSURLRequest *request = [NSURLRequest requestWithURL:self.homeURL];
+	[self.webView loadRequest:request];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+	[super viewWillAppear:animated];
+
+	// Save the NavigationController styles
+	self.navigationControllerStyles = [self.navigationController getStyles];
+
+	[self.navigationController setNavigationBarHidden:NO animated:animated];
+	[self.navigationController setToolbarHidden:NO animated:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+	[super viewWillDisappear:animated];
+	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+	
+	if (self.webView.loading) [self.webView stopLoading];
+	self.webView.delegate = nil;
+
+	// Restore the NavigationController styles
+	[self.navigationController setStyles:self.navigationControllerStyles animated:animated];
+}
+
+- (void)viewDidUnload {
+	self.webView = nil;
+	self.backButtonItem = nil;
+	self.forwardButtonItem = nil;
+	self.refreshButtonItem = nil;
+	self.actionButtonItem = nil;
+	self.spinnerItem = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
+}
+
+#pragma mark Public API
+
+- (NSURL *)currentURL {
+	return [[NSURL URLWithString:[self.webView stringByEvaluatingJavaScriptFromString:@"window.location.href"]] standardizedURL];
+}
+
+- (void)setButtonItemWithSystemItem:(UIBarButtonSystemItem)systemItem type:(CKWebViewControllerButtonItemType)type target:(id)target action:(SEL)action {
+	UIBarButtonItem *item = [[[UIBarButtonItem alloc] initWithBarButtonSystemItem:systemItem target:target action:action] autorelease];
+	[self setBarButtonItem:item forItemType:type target:target action:action];
+}
+
+- (void)setButtonItemWithImage:(UIImage *)image type:(CKWebViewControllerButtonItemType)type target:(id)target action:(SEL)action {
+	if (image == nil)
+		return;
+	UIBarButtonItem *item = [[[UIBarButtonItem alloc] initWithImage:image style:UIBarButtonItemStylePlain target:target action:action] autorelease];
+	[self setBarButtonItem:item forItemType:type target:target action:action];
+}
+
+- (void)setSpinnerStyle:(UIActivityIndicatorViewStyle)style {
+//	self.spinner.activityIndicatorViewStyle = style;
+}
+
+#pragma mark Toolbar Management
+
+- (void)setBarButtonItem:(UIBarButtonItem *)buttonItem forItemType:(CKWebViewControllerButtonItemType)type target:(id)target action:(SEL)action {
+	
+	if (target) buttonItem.target = target;
+	if (action) buttonItem.action = action;
+	
+	switch (type) {
+		case CKWebViewControllerButtonItemBack:
+			self.backButtonItem = buttonItem;
+			self.backButtonItem.target = self;
+			self.backButtonItem.action = @selector(actionGoBack);
+			self.backButtonItem.enabled = NO;
+			break;
+		case CKWebViewControllerButtonItemForward:
+			self.forwardButtonItem = buttonItem;
+			self.forwardButtonItem.target = self;
+			self.forwardButtonItem.action = @selector(actionGoForward);
+			self.forwardButtonItem.enabled = NO;
+			break;
+		case CKWebViewControllerButtonItemRefresh:
+			self.refreshButtonItem = buttonItem;
+			self.refreshButtonItem.target = self;
+			self.refreshButtonItem.action = @selector(actionRefresh);
+			self.refreshButtonItem.enabled = YES;
+			break;
+		case CKWebViewControllerButtonItemAction:
+			self.actionButtonItem = buttonItem;
+			self.actionButtonItem.enabled = YES;
+		default:
+			break;
+	}
+	buttonItem.tag = type;	
+}
+
+- (void)actionGoBack {
+	[self.webView goBack];
+}
+- (void)actionGoForward {
+	[self.webView goForward];
+}
+- (void)actionRefresh {
+	[self.webView reload];
+}
+
+#pragma mark WebView Delegate
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+	[UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+	[self.navigationController.toolbar replaceItemWithTag:CKWebViewControllerButtonItemRefresh withItem:self.spinnerItem];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+
+	// Update the title
+	if (self.showDocumentTitle) {
+		self.title = [self.webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+	}
+
+	[self.navigationController.toolbar replaceItemWithTag:CKWebViewControllerButtonItemRefresh withItem:self.refreshButtonItem];	
+	
+	self.backButtonItem.enabled = self.webView.canGoBack;
+	self.forwardButtonItem.enabled = self.webView.canGoForward;	
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
+@end
