4cee1ef03a5e638ec4f73faafd06462f3dd12479
Fred Brunel <fbrunel@gmail.com>
2010-06-22 16:03:33 -0400

    CKLocation: cache the location of the CLLocationManager before doing any update.
    NOTE: On iOS4, Core Location might not deliver updates if the location is already up-to-date.

--- Classes/CKLocationManager.m
+++ Classes/CKLocationManager.m
@@ -99,7 +99,9 @@
 	_timeoutEnabled = NO;
 
 	CLLocation *currentLocation = _locationManager.location;
-
+	[self setCachedLocation:currentLocation];
+	CKDebugLog(@"Location: %@", _cachedLocation);
+	
 	if (currentLocation == nil) {
 		[_locationManager startUpdatingLocation];
 		return;
