e24e715d243da17e59420a81141763328581981b
Oli Kenobi <oli@olikenobi.com>
2010-06-30 18:31:50 -0400

    Implemented CKImageButtonCellController. Implemented factory methods for CKOverlayView.

---
+++ Classes/CKImageButtonCellController.h
@@ -0,0 +1,20 @@
+//
+//  CKImageButtonCellController.h
+//  CloudKit
+//
+//  Created by Olivier Collet on 10-06-29.
+//  Copyright 2010 WhereCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CKStandardCellController.h"
+
+@interface CKImageButtonCellController : CKStandardCellController {
+	UIImage *_image;
+	UIImage *_highlightedImage;
+	
+}
+
+- (id)initWithTitle:(NSString *)title image:(UIImage *)image hightlightedImage:(UIImage *)hightlightedImage;
+
+@end
---
+++ Classes/CKImageButtonCellController.m
@@ -0,0 +1,67 @@
+//
+//  CKImageButtonCellController.m
+//  CloudKit
+//
+//  Created by Olivier Collet on 10-06-29.
+//  Copyright 2010 WhereCloud Inc. All rights reserved.
+//
+
+#import "CKImageButtonCellController.h"
+#import "CKConstants.h"
+
+
+@interface CKImageButtonCellController ()
+
+@property (nonatomic, retain) UIImage *image;
+@property (nonatomic, retain) UIImage *highlightedImage;
+
+@end
+
+
+@implementation CKImageButtonCellController
+
+@synthesize image = _image;
+@synthesize highlightedImage = _highlightedImage;
+
+- (id)initWithTitle:(NSString *)title image:(UIImage *)image hightlightedImage:(UIImage *)hightlightedImage {
+	if (self = [super initWithText:title]) {
+		self.image = image;
+		self.highlightedImage = hightlightedImage;
+		self.selectable = NO;
+		self.accessoryType = UITableViewCellAccessoryNone;
+	}
+	return self;
+}
+
+- (void)dealloc {
+	self.image = nil;
+	self.highlightedImage = nil;
+	[super dealloc];
+}
+
+- (UITableViewCell *)loadCell {
+	UITableViewCell *cell = [super loadCell];
+
+	UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+	button.tag = 1000;
+	button.frame = cell.contentView.frame;
+	button.autoresizingMask = CKUIViewAutoresizingFlexibleAll;
+	button.titleLabel.font = [UIFont boldSystemFontOfSize:17];
+	[button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+	[cell.contentView addSubview:button];
+
+	return cell;
+}
+
+- (void)setupCell:(UITableViewCell *)cell {
+	[super setupCell:cell];
+	cell.textLabel.text = nil;
+
+	UIButton *button = (UIButton *)[cell.contentView viewWithTag:1000];
+	[button setTitle:self.text forState:UIControlStateNormal];
+	if (self.image) [button setBackgroundImage:self.image forState:UIControlStateNormal];
+	if (self.highlightedImage) [button setBackgroundImage:self.highlightedImage forState:UIControlStateHighlighted];
+	[button addTarget:self.target action:self.action forControlEvents:UIControlEventTouchUpInside];
+}
+
+@end
