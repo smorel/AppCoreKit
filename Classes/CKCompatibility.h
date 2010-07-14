61840981361e6c5b3dff232dc7071cad80add570
Oli Kenobi <oli@olikenobi.com>
2010-07-14 12:19:02 -0400

    CKCompatibility: Added a function to generate a proper CG shadow size.

---
+++ Classes/CKCompatibility.h
@@ -0,0 +1,12 @@
+//
+//  CKCompatibility.h
+//  CloudKit
+//
+//  Created by Olivier Collet on 10-07-14.
+//  Copyright 2010 WhereCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+CGSize CKShadowSizeMake(CGFloat x, CGFloat y);
