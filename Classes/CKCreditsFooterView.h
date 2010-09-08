8bce1b064f64478c8174d6a2c5d3899d151c1f97
Oli Kenobi <oli@olikenobi.com>
2010-09-08 12:43:37 -0400

    CKCreditsFooterView: Table footer view for WhereCloud credits.

---
+++ Classes/CKCreditsFooterView.h
@@ -0,0 +1,18 @@
+//
+//  CKCreditsFooterView.h
+//  CloudKit
+//
+//  Created by Olivier Collet on 10-09-08.
+//  Copyright 2010 WhereCloud Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CKCreditsFooterView : UIView {
+}
+
+- (id)initWithTitle:(NSString *)title;
++ (id)creditsViewWithTitle:(NSString *)title;
+
+@end
