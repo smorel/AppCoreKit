090baf37092a150d7642135c0421b9c8acd96129
Fred Brunel <fred@wherecloud.com>
2011-01-07 12:39:27 -0500

    CKWebRequest2: a new implementation of CKWebRequest not dependent on ASIHTTPRequest.

---
+++ Classes/CKNSObject+Invocation.m
@@ -0,0 +1,47 @@
+//
+//  CKNSObject+Invocation.m
+//  CKWebRequest2
+//
+//  Created by Fred Brunel on 11-01-06.
+//  Copyright 2011 WhereCloud Inc. All rights reserved.
+//
+
+#import "CKNSObject+Invocation.h"
+
+@implementation NSObject (CKNSObjectInvocation)
+
+- (void)performSelectorOnMainThread:(SEL)selector withObject:(id)arg withObject:(id)arg2 waitUntilDone:(BOOL)wait {
+	[self performSelector:selector onThread:[NSThread mainThread] withObjects:[NSArray arrayWithObjects:arg, arg2, nil] waitUntilDone:wait];
+}
+
+- (void)performSelectorOnMainThread:(SEL)selector withObject:(id)arg withObject:(id)arg2 withObject:(id)arg3 waitUntilDone:(BOOL)wait {
+	[self performSelector:selector onThread:[NSThread mainThread] withObjects:[NSArray arrayWithObjects:arg, arg2, arg3, nil] waitUntilDone:wait];
+}
+
+- (void)performSelector:(SEL)selector onThread:(NSThread *)thread withObjects:(NSArray *)args waitUntilDone:(BOOL)wait {
+    if ([self respondsToSelector:selector]) {
+        NSMethodSignature *signature = [self methodSignatureForSelector:selector];
+        if (signature) {
+			NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
+			
+			[invocation setTarget:self];
+			[invocation setSelector:selector];
+				
+			if (args) {
+				NSInteger i = 2;
+				for (NSObject *object in args) {
+					[invocation setArgument:&object atIndex:i++];
+				}
+			}
+				
+			[invocation retainArguments];
+
+			[invocation performSelector:@selector(invoke)
+							   onThread:thread
+							 withObject:nil
+						  waitUntilDone:wait];
+		}
+	}
+}
+
+@end
