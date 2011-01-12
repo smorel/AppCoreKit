85a1648b380ef16fc4db00832cc529bcc0cf6ab4
Fred Brunel <fred@wherecloud.com>
2011-01-12 14:03:32 -0500

    lsnotes.sh: change the pattern matching.

--- Scripts/lsnotes.sh
+++ Scripts/lsnotes.sh
@@ -1,1 +1,4 @@
-git ls-files | xargs grep -n -e "FIXME\|TODO"
+KEYWORDS="TODO:|FIXME:|\?\?\?:|\!\!\!:"
+git ls-files | grep -v Frameworks | grep -e "\\.[h,m]$" | xargs egrep --with-filename --line-number --only-matching "($KEYWORDS).*\$" | perl -p -e "s/($KEYWORDS)/ warning: \$1/"
+
+#git ls-files | xargs grep -n -e "FIXME\|TODO"
