ef502fa8d206f09a52ac5090912a7dc9d338f682
Sebastien Morel <sebastien@wherecloud.com>
2011-02-16 14:05:40 -0500

    CKImageResource : DistantURL vs. Url management.
    Signed-off-by: Sebastien Morel <sebastien@wherecloud.com>

--- CKImageResource.m
+++ CKImageResource.m
@@ -25,42 +25,50 @@ static NSString* isWebUrlRegexString = nil;
 	[super initWithCoder:aDecoder];
 	
 	if(![[NSFileManager defaultManager] fileExistsAtPath:[url absoluteString]] ){
-		url = distantURL;
+		self.url = self.distantURL;
 	}
 	
 	return self;
 }
 
-- (void) encodeWithCoder:(NSCoder *)aCoder {
+- (BOOL)isDistantUrl:(NSURL*)theUrl{
 	if(isWebUrlRegexString == nil){
 		isWebUrlRegexString = @"^(http|https)$";
 	}
 	
-	if(url && [[url scheme] isMatchedByRegex:isWebUrlRegexString]){
+	if(theUrl && [[theUrl scheme] isMatchedByRegex:isWebUrlRegexString])
+		return YES;
+	return NO;
+}
+
+- (void) encodeWithCoder:(NSCoder *)aCoder {
+	if([self isDistantUrl:url]){
 		UIImage* image = [CKImageLoader imageForURL:url];
 		if(image){
-			distantURL = url;
+			self.distantURL = url;
 			NSString* imagePath = [NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.png",[NSString stringWithNewUUID]]];
 			NSData *imageData = [NSData dataWithData:UIImagePNGRepresentation(image)];
 			[imageData writeToFile:imagePath atomically:YES];
-			url = [NSURL URLWithString:imagePath];
+			self.url = [NSURL URLWithString:imagePath];
 		}
 	}
 	
 	[super encodeWithCoder:aCoder];
-	//Save images if we have http urls, modify the url and store http urls in temporary variables
 }
 
-//If 1 of the 2 urls are equal => the resource is considered as the same.
-- (BOOL) isEqual:(id)other {
-	if ([other isKindOfClass:[self class]]) {
-		CKImageResource* otherResource = other;
-		return ([self.url isEqual:otherResource.url] || [self.distantURL isEqual:otherResource.distantURL]
-				|| [otherResource.url isEqual:self.url] || [otherResource.distantURL isEqual:self.distantURL]);
+- (void)setUrl:(NSURL *)theUrl{
+	[url release];
+	url = [theUrl retain];
+	
+	if([self isDistantUrl:url]){
+		self.distantURL = url;
 	}
-	return NO;
 }
 
+- (void)urlMetaData:(CKModelObjectPropertyMetaData*)metaData{
+	metaData.comparable = NO;
+	metaData.hashable = NO;
+}
 
 @end
 
