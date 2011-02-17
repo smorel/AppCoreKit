10fc5fc4623dbd10d94f86227b2973ac19075122
Sebastien Morel <sebastien@wherecloud.com>
2011-02-17 10:44:31 -0500

    Adds CKDataBlockBinder :     This binder allow us to execute a block of code when the observed data will change with the block signature : void doSomething(id newValue);
    Signed-off-by: Sebastien Morel <sebastien@wherecloud.com>

---
+++ Classes/CKDataBlockBinder.h
@@ -0,0 +1,26 @@
+//
+//  CKDataBlockBinder.h
+//  CloudKit
+//
+//  Created by Sebastien Morel on 11-02-17.
+//  Copyright 2011 WhereCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+typedef void(^CKDataExecutionBlock)(id value);
+@interface CKDataBlockBinder : NSObject {
+	id instance;
+	NSString* keyPath;
+	CKDataExecutionBlock executionBlock;
+}
+
+@property (nonatomic, retain) id instance;
+@property (nonatomic, retain) NSString* keyPath;
+@property (nonatomic, copy)   CKDataExecutionBlock executionBlock;
+
++(CKDataBlockBinder*) dataBlockBinder:(id)instance keyPath:(NSString*)keyPath executionBlock:(CKDataExecutionBlock)executionBlock;
+- (void) bind;
+
+@end
