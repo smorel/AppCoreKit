1bdfa839121262957583cd5212f4a57a7b171c3b
Sebastien Morel <sebastien@wherecloud.com>
2011-02-24 18:29:58 -0500

    CKMapping : extension on NSObject that allow to map or initialize an object from a dictionary of values and a mapping dictionary.
    Signed-off-by: Sebastien Morel <sebastien@wherecloud.com>

---
+++ Classes/CKMapping.h
@@ -0,0 +1,46 @@
+//
+//  NFBDataSourceMapper.h
+//  NFB
+//
+//  Created by Sebastien Morel on 11-02-24.
+//  Copyright 2011 WhereCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CloudKit/CKNSObject+Introspection.h>
+
+typedef enum{
+	CKMappingPolicyRequired,
+	CKMappingPolicyOptional
+}CKMappingPolicy;
+
+typedef void(^CKMappingBlock)(id sourceObject,id object,NSString* keyPath,NSError** error);
+@interface CKMapping : NSObject{
+	NSString* key;
+	CKMappingBlock mapperBlock;
+	CKMappingPolicy policy;
+}
+
+@property (nonatomic, retain) NSString *key;
+@property (nonatomic, copy) CKMappingBlock mapperBlock;
+@property (nonatomic, assign) CKMappingPolicy policy;
+
+@end
+
+
+@interface NSObject (CKMapping) 
+
+- (id)initWithDictionary:(NSDictionary*)sourceDictionary withMappings:(NSMutableDictionary*)mappings error:(NSError**)error;
+- (void)mapWithDictionary:(NSDictionary*)sourceDictionary withMappings:(NSMutableDictionary*)mappings error:(NSError**)error;
+
+@end
+
+
+@interface NSMutableDictionary (CKMapping)
+- (void)mapKeyPath:(NSString*)keyPath toKeyPath:(NSString*)destination required:(BOOL)bo withBlock:(CKMappingBlock)block;
+- (void)mapURLForKeyPath:(NSString*)keyPath toKeyPath:(NSString*)destination required:(BOOL)bo;
+- (void)mapStringForKeyPath:(NSString*)keyPath toKeyPath:(NSString*)destination required:(BOOL)bo;
+- (void)mapStringWithoutHTMLForKeyPath:(NSString*)keyPath toKeyPath:(NSString*)destination required:(BOOL)bo;
+- (void)mapTrimmedStringForKeyPath:(NSString*)keyPath toKeyPath:(NSString*)destination required:(BOOL)bo;
+- (void)mapIntForKeyPath:(NSString*)keyPath toKeyPath:(NSString*)destination required:(BOOL)bo;
+@end
---
+++ Classes/CKNSString+Parsing.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Parsing.h
+//  NFB
+//
+//  Created by Jean-Philippe Martin on 11-02-04.
+//  Copyright 2011 WhereCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSString (Parsing) 
+
+- (NSString *)stringByDeletingHTMLTags;
+
+@end
