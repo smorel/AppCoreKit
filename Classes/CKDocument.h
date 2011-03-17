5040a33c5631f63e3cb0c6d9cbb231febbd35890
Sebastien Morel <sebastien@wherecloud.com>
2011-03-17 14:42:42 -0400

    CKDocument adds a addObjects atIndex method.
    Signed-off-by: Sebastien Morel <sebastien@wherecloud.com>

--- Classes/CKDocument.h
+++ Classes/CKDocument.h
@@ -11,6 +11,7 @@
 
 - (NSArray*)objectsForKey:(NSString*)key;
 - (void)addObjects:(NSArray*)newItems forKey:(NSString*)key;
+- (void)addObjects:(NSArray*)newItems atIndex:(NSUInteger)index forKey:(NSString*)key;
 - (void)removeObjects:(NSArray*)items forKey:(NSString*)key;
 - (void)removeAllObjectsForKey:(NSString*)key;
 - (void)addObserver:(id)object forKey:(NSString*)key;
