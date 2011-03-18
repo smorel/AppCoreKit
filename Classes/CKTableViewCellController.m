9ef61d7129e48c9a365a9d973f0327404b32569d
Sebastien Morel <sebastien@wherecloud.com>
2011-03-18 18:18:41 -0400

    CKTableViewCellController : Changed CKManagedTableViewController to CKTableViewController.
    Signed-off-by: Sebastien Morel <sebastien@wherecloud.com>

--- Classes/CKTableViewCellController.m
+++ Classes/CKTableViewCellController.m
@@ -60,7 +60,7 @@
 	_indexPath = [indexPath retain];
 }
 
-- (void)setParentController:(CKManagedTableViewController *)parentController {
+- (void)setParentController:(CKTableViewController *)parentController {
 	// Set a *weak* reference to the parent controller
 	// This method is hidden from the public interface and is called by the CKManagedTableViewController
 	// when adding the CKTableViewCellController.
