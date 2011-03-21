b6c74897a04b95b85cf3ff7b79dd2e0e7f5228ad
Sebastien Morel <sebastien@wherecloud.com>
2011-03-21 13:03:05 -0400

    Adds : CKObjectController, CKDocumentController, CKObjectViewControllerFactory and CKObjectTableViewController.
    Signed-off-by: Sebastien Morel <sebastien@wherecloud.com>

---
+++ Classes/CKDocumentController.h
@@ -0,0 +1,26 @@
+//
+//  CKFeedController.h
+//  FeedView
+//
+//  Created by Sebastien Morel on 11-03-16.
+//  Copyright 2011 WhereCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CKObjectController.h"
+#import <CloudKit/CKModelObserver.h>
+
+
+@interface CKDocumentController : NSObject<CKObjectController> {
+	id _document;
+	NSString* _key;
+	id _delegate;
+}
+
+@property (nonatomic, assign) id document;
+@property (nonatomic, retain) NSString* key;
+@property (nonatomic, assign) id delegate;
+
+- (id)initWithDocument:(id)document key:(NSString*)key;
+
+@end
---
+++ Classes/CKNSDictionary+TableViewAttributes.m
@@ -0,0 +1,44 @@
+//
+//  CKNSDictionary+TableView.m
+//  FeedView
+//
+//  Created by Sebastien Morel on 11-03-18.
+//  Copyright 2011 WhereCloud Inc. All rights reserved.
+//
+
+#import "CKNSDictionary+TableViewAttributes.h"
+
+NSString* const CKTableViewAttributeBounds = @"CKTableViewAttributeBounds";
+NSString* const CKTableViewAttributePagingEnabled = @"CKTableViewAttributePagingEnabled";
+NSString* const CKTableViewAttributeInterfaceOrientation = @"CKTableViewAttributeInterfaceOrientation";
+NSString* const CKTableViewAttributeOrientation = @"CKTableViewAttributeOrientation";
+NSString* const CKTableViewAttributeAnimationDuration = @"CKTableViewAttributeAnimationDuration";
+
+@implementation NSDictionary (CKTableViewAttributes)
+
+- (BOOL)pagingEnabled{
+	NSNumber* v = (NSNumber*)[self objectForKey:CKTableViewAttributePagingEnabled];
+	return v ? [v boolValue] : NO;
+}
+
+- (UIInterfaceOrientation)interfaceOrientation{
+	NSNumber* v = (NSNumber*)[self objectForKey:CKTableViewAttributeInterfaceOrientation];
+	return (UIInterfaceOrientation)v ? [v intValue] : UIInterfaceOrientationPortrait;
+}
+
+- (CGSize)bounds{
+	NSValue* v = (NSValue*)[self objectForKey:CKTableViewAttributeBounds];
+	return v ? [v CGSizeValue] : CGSizeMake(0, 0);
+}
+
+- (CKTableViewOrientation)tableOrientation{
+	NSNumber* v = (NSNumber*)[self objectForKey:CKTableViewAttributeOrientation];
+	return (CKTableViewOrientation)v ? [v intValue] : CKTableViewOrientationPortrait;
+}
+
+- (NSTimeInterval)animationDuration{
+	NSNumber* v = (NSNumber*)[self objectForKey:CKTableViewAttributeAnimationDuration];
+	return (NSTimeInterval) (v ? [v doubleValue] : 0.0);
+}
+
+@end
