2b50f51620f5a6cc87dbf73632f861d08b8a24f3
Sebastien Morel <sebastien@wherecloud.com>
2011-06-28 14:21:59 -0400

    Adds a scheme to build only the documentation and copy it to the XCode documentation folder. By this way, we can comment the code, build the documentation and see the changes directly reflected in the xcode quick help and documentation ! Moreover this does'nt compile any code so it's quick.
    Signed-off-by: Sebastien Morel <sebastien@wherecloud.com>

--- Classes/CKWeakRef.h
+++ Classes/CKWeakRef.h
@@ -9,13 +9,14 @@
 #import <Foundation/Foundation.h>
 #import "CKCallback.h"
 
-/** Weak reference with dealloc callback mechanism.
-  * A CKWeakRef allow to reference objects in a weak way and setup a callback that will get called when the target object is deallocated.
-  *
-  *         CKWeakRef* ref = [CKWeakRef weakRefWithObject:theObject block:^(id object){
-  *             //Do something when theObject is deallocated
-  *         }];
-  */
+/** 
+ Weak reference with dealloc callback mechanism. A CKWeakRef allow to reference an object without retaining it. You can optionally register a CKCallback to execute some code when the object is deallocated. When the object is deallocated, the object property of the CKWeakRef is set to nil. By this way, you can safelly call [myRef object] with no crash.
+ 
+ CKWeakRef* ref = [CKWeakRef weakRefWithObject:theObject block:^(id object){
+ //Do something when theObject is deallocated
+ }];
+ 
+ */
 @interface CKWeakRef : NSObject {
 	id _object;
 	CKCallback* _callback;
@@ -30,18 +31,61 @@
 ///-----------------------------------
 
 /** test
+ @param object The target.
+ @see object
  */
 - (id)initWithObject:(id)object;
 
-/** tests2
+/** test
+ @param object The target.
+ @param callback The callback to execute when object is deallocated.
+ @see object
+ @see CKCallback
  */
 - (id)initWithObject:(id)object callback:(CKCallback*)callback;
+
+/** test
+ @param object The target.
+ @param block The block to execute when object is deallocated.
+ @see object
+ */
 - (id)initWithObject:(id)object block:(void (^)(id object))block;
+
+/** test
+ @param object The target.
+ @param target The target on wich action will be executed when object is deallocated.
+ @param action The selector to execute on target when object is deallocated.
+ @see object
+ */
 - (id)initWithObject:(id)object target:(id)target action:(SEL)action;
 
+/** test
+ @param object The target.
+ @see object
+ */
 + (CKWeakRef*)weakRefWithObject:(id)object;
+
+/** test
+ @param object The target.
+ @param callback The callback to execute when object is deallocated.
+ @see object
+ @see CKCallback
+ */
 + (CKWeakRef*)weakRefWithObject:(id)object callback:(CKCallback*)callback;
+
+/** test
+ @param object The target.
+ @param block The block to execute when object is deallocated.
+ @see object
+ */
 + (CKWeakRef*)weakRefWithObject:(id)object block:(void (^)(id object))block;
+
+/** test
+ @param object The target.
+ @param target The target on wich action will be executed when object is deallocated.
+ @param action The selector to execute on target when object is deallocated.
+ @see object
+ */
 + (CKWeakRef*)weakRefWithObject:(id)object target:(id)target action:(SEL)action;
 
 @end
