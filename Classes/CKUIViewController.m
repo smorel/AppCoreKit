c036d8afec96680b7aa91d7974f8f3c364210a05
Sebastien Morel <sebastien@wherecloud.com>
2011-08-22 16:12:38 -0400

    CKUIViewController : adds a copy block helper check in callstack when retaining controllers. It can be enabled in the app plist by setting CKDebugCheckForBlockCopy to YES.
    Signed-off-by: Sebastien Morel <sebastien@wherecloud.com>

--- Classes/CKUIViewController.m
+++ Classes/CKUIViewController.m
@@ -8,7 +8,16 @@
 
 #import "CKUIViewController.h"
 #import "CKUIViewController+Style.h"
+#import "CKDebug.h"
+#include <execinfo.h>
 
+typedef enum CKDebugCheckForBlockCopyState{
+    CKDebugCheckForBlockCopyState_none,
+    CKDebugCheckForBlockCopyState_NO,
+    CKDebugCheckForBlockCopyState_YES
+}CKDebugCheckForBlockCopyState;
+
+static CKDebugCheckForBlockCopyState CKDebugCheckForBlockCopyCurrentState = CKDebugCheckForBlockCopyState_none;
 
 @implementation CKUIViewController
 @synthesize name = _name;
@@ -54,6 +63,34 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
 	return YES;
+}
+
+- (void)CheckForBlockCopy{
+    if(CKDebugCheckForBlockCopyCurrentState == CKDebugCheckForBlockCopyState_none){
+        BOOL bo = [[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CKDebugCheckForBlockCopy"]boolValue];
+        CKDebugCheckForBlockCopyCurrentState = bo ? CKDebugCheckForBlockCopyState_YES : CKDebugCheckForBlockCopyState_NO;
+    }
+    
+    if(CKDebugCheckForBlockCopyCurrentState != CKDebugCheckForBlockCopyState_YES)
+        return;
+    
+    void *frames[128];
+    int len = backtrace(frames, 128);
+    char **symbols = backtrace_symbols(frames, len);
+    for (int i = 0; i < len; ++i) {
+        NSString* string = [NSString stringWithUTF8String:symbols[i]];
+        NSRange range = [string rangeOfString:@"__copy_helper_block_"];
+        if(range.location != NSNotFound){
+            NSAssert(NO,@"You are retaining an object in a block copy !\nPlease define a variable with __block %@* bYourVar = yourVar; outside the scope of the block and use bYourVar in your block instead of yourVar.",[self class]);
+        }
+    }
+    free(symbols);
+}
+
+
+- (id)retain{
+    [self CheckForBlockCopy];
+    return [super retain];
 }
 
 @end
