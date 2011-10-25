39556cd595ef2fc252010bc882d62cf6b1e74880
Sebastien Morel <sebastien@wherecloud.com>
2011-10-25 11:18:18 -0400

    FIXME comments.

--- Classes/CKNSDatePropertyCellController.m
+++ Classes/CKNSDatePropertyCellController.m
@@ -355,6 +355,8 @@ static NSMutableDictionary* CKNSDateSheetControllersSingleton = nil;
             
             if(_enableAccessoryView){
                 UINavigationController* navController = [[[UINavigationController alloc]initWithRootViewController:dateController]autorelease];
+                
+                //FIXME : This has been commented because it offsets the view up and she appears behind the navigation bar.
                 //navController.navigationBar.barStyle = UIBarStyleBlackTranslucent;
                 sheetController = [[CKSheetController alloc]initWithContentViewController:navController];
             }
