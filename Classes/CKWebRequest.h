00ae1253d791f051dfdec72bf97923cad57245ad
gcamp <guillaume.campagna@gmail.com>
2012-05-24 09:43:32 -0400

    Add some comments

--- Classes/CKWebRequest.h
+++ Classes/CKWebRequest.h
@@ -14,13 +14,13 @@ OBJC_EXPORT NSString * const CKWebRequestHTTPErrorDomain;
 
 @property (nonatomic, readonly) NSURL *URL;
 @property (nonatomic, copy) void (^completionBlock)(id response, NSHTTPURLResponse *urlResponse, NSError *error);
-@property (nonatomic, copy) id (^transformBlock)(id value);
+@property (nonatomic, copy) id (^transformBlock)(id value); //Called to apply a possible transformation to the response before the completion block
 
-@property (nonatomic, assign) id<NSURLConnectionDelegate, NSURLConnectionDataDelegate> delegate;//Forward URL connection if nessesary
-@property (nonatomic, retain, readonly) NSString *downloadPath;
-@property (nonatomic, readonly) CGFloat progress;
+@property (nonatomic, assign) id<NSURLConnectionDelegate, NSURLConnectionDataDelegate> delegate; //Forward URL connection delegate if nessesary
+@property (nonatomic, retain, readonly) NSString *downloadPath; //Need to be set at initialization 
+@property (nonatomic, readonly) CGFloat progress; //KVO compilent
 
-//See CKWebRequest+Initialization.h for other init methods
+//See CKWebRequest+Initialization.h for other init and convinience methods
 - (id)initWithURLRequest:(NSURLRequest*)request parameters:(NSDictionary*)parameters transform:(id (^)(id value))transform completion:(void (^)(id object, NSHTTPURLResponse *response, NSError *error))block;
 - (id)initWithURLRequest:(NSURLRequest*)request parameters:(NSDictionary*)parameters downloadAtPath:(NSString*)path completion:(void (^)(id object, NSHTTPURLResponse *response, NSError *error))block;
 
