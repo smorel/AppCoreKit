d6767429d9d2cdc5a6430a32baa85154a4d186aa
gcamp <guillaume.campagna@gmail.com>
2012-05-31 13:34:21 -0400

    Use __gnu_cxx::hash_map instead of NSDictionnary with CKPropertyExtendedAttributesPerThreadSingleton

---
+++ Classes/CKPropertyExtendedAttributes.mm
@@ -0,0 +1,63 @@
+//
+//  CKPropertyExtendedAttributes.m
+//  CloudKit
+//
+//  Created by Sebastien Morel on 11-08-12.
+//  Copyright 2011 Wherecloud. All rights reserved.
+//
+
+#import "CKPropertyExtendedAttributes.h"
+
+#include <ext/hash_map>
+
+using namespace __gnu_cxx;
+
+namespace __gnu_cxx{
+    template<> struct hash< NSThread* >
+    {
+        size_t operator()( NSThread* x ) const{
+            return (size_t)x;
+        }
+    };
+}
+
+static __gnu_cxx::hash_map<NSThread*, CKPropertyExtendedAttributes*> CKPropertyExtendedAttributesPerThreadSingleton;
+
+@implementation CKPropertyExtendedAttributes
+@synthesize attributes;
+
+- (id)init{
+    self = [super init];
+    self.attributes = [NSMutableDictionary dictionary];
+    return self;
+}
+
+- (void)dealloc{
+	self.attributes = nil;
+	[super dealloc];
+}
+
+- (void)reset{
+	[self.attributes removeAllObjects];
+}
+
++ (CKPropertyExtendedAttributes*)extendedAttributesForObject:(id)object property:(CKClassPropertyDescriptor*)property{
+    NSThread* currentThread = [NSThread currentThread];
+    
+    CKPropertyExtendedAttributes* attributes = CKPropertyExtendedAttributesPerThreadSingleton[currentThread];
+    if(!attributes){
+        attributes = [[CKPropertyExtendedAttributes alloc]init];
+        CKPropertyExtendedAttributesPerThreadSingleton[currentThread] = attributes;
+    }
+    
+	[attributes reset];
+	
+	SEL extendedAttributesSelector = property.extendedAttributesSelector;
+	if([object respondsToSelector:extendedAttributesSelector]){
+		[object performSelector:extendedAttributesSelector withObject:attributes];
+	}
+	
+	return attributes;
+}
+
+@end
