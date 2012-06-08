280a22273bfabc228a8f9e78326f6dc671a3a5d7
smorel <sebastien@wherecloud.com>
2012-06-08 14:31:45 -0400

    Adds factory methods for requests with url,mapping,params,body and completion/error blocks.

---
+++ Classes/CKWebRequest+Mapping.h
@@ -0,0 +1,30 @@
+//
+//  CKWebRequest+Mapping.h
+//  CloudKit
+//
+//  Created by Martin Dufort on 12-06-08.
+//  Copyright (c) 2012 Wherecloud. All rights reserved.
+//
+
+#import "CKWebRequest.h"
+
+@interface CKWebRequest (StandardRequests)
+
++ (CKWebRequest*) requestForObjectsWithUrl:(NSURL*)url
+                                    params:(NSDictionary*)params
+                                      body:(NSData*)body
+                  mappingContextIdentifier:(NSString*)mappingIdentifier
+                          transformRawData:(NSArray*(^)(id value))transformRawDataBlock
+                                completion:(void(^)(NSArray* objects))completionBlock 
+                                     error:(void(^)(id value, NSHTTPURLResponse* response, NSError* error))errorBlock;
+
++ (CKWebRequest*)requestForObject:(id)object
+                              url:(NSURL*)url
+                           params:(NSDictionary*)params
+                             body:(NSData*)body
+         mappingContextIdentifier:(NSString*)identifier
+                 transformRawData:(NSDictionary*(^)(id value))transformRawDataBlock
+                       completion:(void(^)(id object))completionBlock 
+                            error:(void(^)(id value, NSHTTPURLResponse* response, NSError* error))errorBlock;
+
+@end
