b35b50c993def3a569a3cb5f31fa8cdace2383ce
smorel <sebastien@wherecloud.com>
2012-11-12 13:42:47 -0500

    Adds pragma compilation message when compiling with distribution enabled.

--- Classes/CKLicense.mm
+++ Classes/CKLicense.mm
@@ -14,11 +14,19 @@
 @implementation CKLicense
 @end
 
+#ifndef DISTRIBUTION
+#pragma message( "Compiling CKLicense by bypassing License check." )
+#endif
+
 #ifdef DISTRIBUTION
+
+#pragma message( "Compiling CKLicense by enabling License check." ) 
 
 static const char pub_key[] = {"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyjapahE9WLWaumlDcPJJ\nyql1xeMF3IZh123wFLW0E6K5Twgbg7aFzCnZCLj4j6n606m7OWWKNCHEAGO/68e4\nGRL+k2237iCLeDc4cGv94sENiBcJbt0lVTiLZbS8j7P/v58Cc83bXlRIwmKHyS4C\n1JnSeAPLsEbblKsgkEyX/xEgQYK8G2hhpdotMtKM2ltlvSE2PBSSE+61qxRaBQHy\nkgW3JUA9uDcxwXCRLT7AzC8MNqQk89pCPFEk6sgHslrFjQEIj54vEJBbDsbHnjON\n9wE6zAG23H/3i/zmmgWn/ueHmxGkxMbEFmKn8YllUZ3vDA1hffjyxsYv47qhtSye\nkQIDAQAB\n-----END PUBLIC KEY-----"};
 
 bool checkLicense(){
+    printf("Checking AppCoreKit License.\n");
+    
     @autoreleasepool {
         Class delegateClass = [CKLicense class];
         if([delegateClass respondsToSelector:@selector(licenseKey)]){
