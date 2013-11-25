e03f0ab29ad0cc1acd3e2c0d99bd080765a2cf93
Nicolas Clapiès <nclapies@wherecloud.com>
2013-11-25 12:31:57 -0500

    Adding runtime extension on UIControl to manage contentHorizontalAlignment & contentVerticalAlignment textual enum conversions

---
+++ Classes/UIControl+Introspection.h
@@ -0,0 +1,13 @@
+//
+//  UIControl+Introspection.h
+//  AppCoreKit
+//
+//  Created by Nicolas Clapies on 11/25/2013.
+//  Copyright (c) 2013 Wherecloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIControl (Introspection)
+
+@end
