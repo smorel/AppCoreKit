0a733bff013aabbe0b92c5b4a40e4505e25638ec
smorel <sebastien@wherecloud.com>
2014-02-07 15:39:02 -0500

    UIPickerView : Support for AppCoreKit layout with default metrics by os versions.

---
+++ Classes/Layout/Public/Additions/UIKit/UIPickerView+CKLayout.h
@@ -0,0 +1,13 @@
+//
+//  UIPickerView+CKLayout.h
+//  AppCoreKit
+//
+//  Created by Sebastien Morel on 1/29/2014.
+//  Copyright (c) 2014 Wherecloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIPickerView (CKLayout)
+
+@end
---
+++ Classes/Layout/Public/Additions/UIKit/UIPickerView+CKLayout.m
@@ -0,0 +1,49 @@
+//
+//  UIPickerView+CKLayout.m
+//  AppCoreKit
+//
+//  Created by Sebastien Morel on 1/29/2014.
+//  Copyright (c) 2014 Wherecloud. All rights reserved.
+//
+
+#import "UIPickerView+CKLayout.h"
+
+#import <objc/runtime.h>
+#import <QuartzCore/QuartzCore.h>
+#import "CKPropertyExtendedAttributes.h"
+#import "CKStyleManager.h"
+#import "CKRuntime.h"
+#import "UIView+Name.h"
+#import "CKStyleView.h"
+
+#import "UIView+CKLayout.h"
+#import "CKRuntime.h"
+
+
+@interface CKLayoutBox()
+
++ (CGSize)preferedSizeConstraintToSize:(CGSize)size forBox:(NSObject<CKLayoutBoxProtocol>*)box;
+
+@end
+
+@implementation UIPickerView (CKLayout)
+
+- (CGSize)preferedSizeConstraintToSize:(CGSize)size{
+    if(CGSizeEqualToSize(size, self.lastComputedSize))
+        return self.lastPreferedSize;
+    
+    self.lastComputedSize = size;
+    
+    size.width -= self.padding.left + self.padding.right;
+    size.height -= self.padding.top + self.padding.bottom;
+    
+    CGSize ret = [self sizeThatFits:size];
+    ret.height = MAX(162.0, ret.height);
+
+    ret = [CKLayoutBox preferedSizeConstraintToSize:ret forBox:self];
+    
+    self.lastPreferedSize = CGSizeMake(MIN(size.width,ret.width) + self.padding.left + self.padding.right,MIN(size.height,ret.height) + self.padding.top + self.padding.bottom);
+    return self.lastPreferedSize;
+}
+
+@end
