bcc36e307f1acc02ac0b027f50fd0da6efa22dc4
Sebastien Morel <sebastien@mightycast.com>
2015-05-05 16:52:09 -0400

    pre-computing runtime property descriptors for the classes that are mostly used at launch.

--- Classes/Foundation/Private/CKRuntimeBootstrap.mm
+++ Classes/Foundation/Private/CKRuntimeBootstrap.mm
@@ -6,8 +6,26 @@
 //  Copyright (c) 2015 Wherecloud. All rights reserved.
 //
 
-#import "CKRuntimeBootstrap.h"
+#import <UIKit/UIKit.h>
+#import "NSObject+Runtime.h"
+#import "NSObject+Runtime_private.h"
+#import "CKClassPropertyDescriptor_private.h"
 
-@implementation CKRuntimeBootstrap
+/** We precompute descriptors at launch to speed up application at runtime.
+ */
+bool precompute_runtime_descriptors(){
+    NSArray* AppCoreKitClasses = [NSObject allClassesWithPrefix:@"CK"];
+    NSArray* ViewsClasses = [NSObject allClassesKindOfClass:[UIView class]];
+    
+    for(Class c in AppCoreKitClasses){
+        [[CKClassPropertyDescriptorManager defaultManager]allPropertiesForClass:c];
+    }
+    
+    for(Class c in ViewsClasses){
+        [[CKClassPropertyDescriptorManager defaultManager]allPropertiesForClass:c];
+    }
+    
+    return true;
+}
 
-@end
+static bool bo_precompute_runtime_descriptors = precompute_runtime_descriptors();
