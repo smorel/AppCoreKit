68f0e1af59dc2f4457649b1bc5e646ecaf15b087
Sebastien Morel <sebastien@mightycast.com>
2015-06-26 10:50:09 -0400

    cocoapods build

--- Classes/Layout/Public/Additions/UIKit/UIViewController+CKLayout.h
+++ Classes/Layout/Public/Additions/UIKit/UIViewController+CKLayout.h
@@ -8,6 +8,7 @@
 
 #import "CKLayoutBoxProtocol.h"
 #import "CKLayoutBox.h"
+#import "UIViewController+Style.h"
 
 /**
  */
@@ -15,31 +16,3 @@
 @end
 
 
-/**
- */
-@interface UIViewController (CKContainerViewController)
-
-///-----------------------------------
-/// @name Accessing the Container Controller
-///-----------------------------------
-
-/**
-*/
-@property(nonatomic,retain) NSString* name;
-
-
-/**
- */
-@property (nonatomic,assign) UIViewController *containerViewController;
-
-/** returns the container controller of the specified class in the container hierarchy.
- returns nil if no container controller matching the specified type;
- */
-- (UIViewController*)containerViewControllerOfClass:(Class)type;
-
-/** returns the container controller conforms to the specified protocol in the container hierarchy.
- returns nil if no container controller conforming to the specified protocol;
- */
-- (UIViewController*)containerViewControllerConformsToProtocol:(Protocol*)protocol;
-
-@end
--- Classes/Styles/Public/UIViewController+Style.h
+++ Classes/Styles/Public/UIViewController+Style.h
@@ -36,3 +36,33 @@
 - (NSMutableDictionary*)applyStyleWithParentStyle:(NSMutableDictionary*)style;
 
 @end
+
+
+/**
+ */
+@interface UIViewController (CKContainerViewController)
+
+///-----------------------------------
+/// @name Accessing the Container Controller
+///-----------------------------------
+
+/**
+ */
+@property(nonatomic,retain) NSString* name;
+
+
+/**
+ */
+@property (nonatomic,assign) UIViewController *containerViewController;
+
+/** returns the container controller of the specified class in the container hierarchy.
+ returns nil if no container controller matching the specified type;
+ */
+- (UIViewController*)containerViewControllerOfClass:(Class)type;
+
+/** returns the container controller conforms to the specified protocol in the container hierarchy.
+ returns nil if no container controller conforming to the specified protocol;
+ */
+- (UIViewController*)containerViewControllerConformsToProtocol:(Protocol*)protocol;
+
+@end
